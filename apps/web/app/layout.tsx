4abb8390ad206eccd67ae19dc3d88466884b81eb
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-08-26 11:14:16 -0400

    Added a Header with custom syles from styles package

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { container } from  "styles/header.css"
 import  "styles/globals.css"
 
 export default function RootLayout({
@@ -7,7 +9,14 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* The container class is a string that has been converted into a class. When it is used here, all the vanilla extract stuff are gone  */}
+        <div className={container}>
+          <Image src="/react.png" width={25} height={25} alt="ReactJS Logo" />
+          <div className="">NextJS App Router</div>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
